635e160aa4bb35422b8ca0d68e2301d41ab17438
Shreyash Royzada <65035990+ShreyashRoyzada@users.noreply.github.com>
2020-11-01 20:27:04 +0530

    Update Calculations.cpp

--- Calculations.cpp
+++ Calculations.cpp
@@ -2,9 +2,7 @@
 using namespace std;
 
 int main() {
-	// your code goes here
-	int ch = 0;
-
+	
 	float MCbore=0,FCNoOfPots=0,FCDia=0,RCNoOfPots=0,RCDia=0,FTrad=0,RTRad=0,COMfront=0,COMrear=0,COMheight=0,Mass=0,FBDDia=0,RBDDia=0,Fd=0;
 	cout<<"Enter the following data in SI Units -\n 1. Master Cylinder Bore Dia\n 2. Front caliper number of pots \n 3. Front caliper pot diameter \n 4. Rear caliper number of pots \n 5. Rear caliper pot diameter \n 6.Tyre Radius front \n 7. Tyre Radius rear \n 8. COM Distance from front axle \n 9. COM Distance from rear axle \n 10. COM Height from ground \n 11. Mass of the vehicle(including Safety Factor) \n 12. Front Brake disc Diameter \n 13. Rear Brake Disc Diameter \n 14. Applied Force by Driver";
 	const float fricTyreRoad = 0.6,fricPadsDisc = 0.4;
